65a920b1a910d3f6ea7a184d8f0fcfd7b62c23a7
Abhishek Sachan <140067029+abhishek8756@users.noreply.github.com>
2024-08-08 17:03:56 +0530

    script.js

--- script.js
+++ script.js
@@ -1,58 +1,70 @@
-// Assigning the initial values of milliseconds, seconds, minutes and hour as 0, 0, 0, 0 using an array
-let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-// select the displayTime and store it in a local variable
-let displayTime = document.getElementById("displayTime");
-// it shows that no interval is present or started
-let timer = null;
+//initializing variables
+let start = document.getElementById('start');
+let stop = document.getElementById('stop');
+let reset = document.getElementById('reset');
+let hour = 0;
+let minute = 0;
+let second = 0;
+let count = 0;
+let counter = true;
+let startWatch=true;
+//click on start button
+start.addEventListener('click', () => {
+    counter = true;
+    if(startWatch){
+    stopWatch();
+    startWatch=false;
+    }
+   
+});
+//click on stop button
+stop.addEventListener('click', () => {
+    counter = false;
+    startWatch=true;
+});
+//click on reset button
+reset.addEventListener('click', () => {
+    startWatch=true;
+    counter = false;
+    hour = 0;
+    minute = 0;
+    second = 0;
+    count = 0;
+    document.getElementById('hr').innerHTML = '00';
+    document.getElementById('min').innerHTML = '00';
+    document.getElementById('sec').innerHTML = '00';
 
-// create a function for the StopWatch
-function stopWatch(){
-    milliseconds += 10; // increase milliseconds by 10 after each encounter
-    if(milliseconds == 1000){ //when milliseconds reaches to 1000 change it to 00 and increase the secondsby 1
-        milliseconds = 00;
-        seconds++;
-        if(seconds == 60){ //when seconds reaches to 60, change it to 0 and increase minutes by 1
-            seconds = 0;
-            minutes++;
-            if(minutes == 60){ //when minutes reaches to 60, change it to 0 and increase hours by 1
-                minutes = 0;
-                hours++;
-            }
+
+});
+//main functioning of timer
+function stopWatch() {
+    if (counter) {
+        second++;
+        if (second == 60) {
+            minute++;
+            second = 0;
         }
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+            second = 0;
+        }
+        let hrString = hour;
+        let minString = minute;
+        let secString = second;
+        if (hour < 10) {
+            hrString = '0' + hrString;
+        }
+        if (minute < 10) {
+            minString = '0' + minString;
+        }
+        if (second < 10) {
+            secString = '0' + secString;
+        }
+        document.getElementById('hr').innerHTML = hrString;
+        document.getElementById('min').innerHTML = minString;
+        document.getElementById('sec').innerHTML = secString;
+
+        setTimeout(stopWatch, 1000);
     }
-
-    // if hour is less than 10 add a 0 prior to hours and store it in a local variable for furture use
-    let h = hours < 10 ? "0" + hours : hours;
-    // if minutes is less than 10 add a 0 prior to minutes and store it in a local variable for furture use
-    let m = minutes < 10 ? "0" + minutes : minutes;
-    // if seconds is less than 10 add a 0 prior to seconds and store it in a local variable for furture use
-    let s = seconds < 10 ? "0" + seconds : seconds;
-    // if milliseconds is less than 10 add a 0 prior to milliseconds and store it in a local variable for furture use
-    let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
-
-    // display the latest time
-    displayTime.innerHTML = h + ":" + m + ":" + s + "." + ms;
-}
-
-// create a function to start the StopWatch
-function watchStart(){
-    // if any ongoing interval is present (or running) stop it and then restart it 
-    if(timer !== null){
-        clearInterval(timer);
-    }
-    timer = setInterval(stopWatch, 10);
-}
-
-// create a function to stop the StopWatch
-function watchStop(){
-    // clear the previous interval
-    clearInterval(timer);
-}
-
-// create a function to reset the StopWatch
-function watchReset(){
-    clearInterval(timer);
-    // clear the previous interval and assign the default values to the timer 
-    [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
-    displayTime.innerHTML = "00:00:00.000"
 }
